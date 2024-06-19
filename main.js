6c360114e02f78e214276d3ec3896c5732b81da6
“[tabin <[erphan2018@gmail.com]>
2024-06-19 22:11:16 +0500

    second commit

---
+++ main.js
@@ -0,0 +1,9 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: "enter your sentence to count the word :"
+    }]);
+const words = answer.sentence.trim().split("  ");
+console.log(words);
+console.log(`your sentence word count is ${words.length}`);
